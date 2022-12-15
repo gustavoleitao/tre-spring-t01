fb3dfea0568a1cfb0d3d78b2d859ea20d34a4af9
Gustavo Leitão <gustavo.leitao@imd.ufrn.br>
2022-12-15 11:36:56 -0300

    A ç

---
+++ src/main/java/br/ufrn/imd/treeleicao/HelloController.java
@@ -0,0 +1,14 @@
+package br.ufrn.imd.treeleicao;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello(){
+        return "Hello World!!!";
+    }
+
+}
