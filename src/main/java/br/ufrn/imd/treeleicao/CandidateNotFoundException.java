58418780493de346e50f2911193ba970606d6701
Gustavo Leitão <gustavo.leitao@imd.ufrn.br>
2022-12-15 11:46:56 -0300

    pt02

---
+++ src/main/java/br/ufrn/imd/treeleicao/CandidateNotFoundException.java
@@ -0,0 +1,2 @@
+package br.ufrn.imd.treeleicao;public class CandidateNotFoundException {
+}
--- src/main/java/br/ufrn/imd/treeleicao/model/Error.java
+++
@@ -1,3 +0,0 @@
-package br.ufrn.imd.treeleicao.model;
-
-public record Error (int code, String message){}
