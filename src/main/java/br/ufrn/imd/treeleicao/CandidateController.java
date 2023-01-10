3be01e2f0ee04ade37199fd8b3b3e2391dcce9d3
Gustavo Leitão <gustavo.leitao@imd.ufrn.br>
2023-01-10 11:37:05 -0300

    Adiciona tratamento de exceções

--- src/main/java/br/ufrn/imd/treeleicao/CandidateController.java
+++ src/main/java/br/ufrn/imd/treeleicao/CandidateController.java
@@ -1,7 +1,11 @@
 package br.ufrn.imd.treeleicao;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.Collection;
 
 @RestController
